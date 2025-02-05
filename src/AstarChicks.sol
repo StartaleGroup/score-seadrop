1016deb7b52facc0e674a232c7856d0dd685017c
Mar.io <34627453+Maar-io@users.noreply.github.com>
2025-02-05 20:25:28 +0100

    deployed contrat has no tokenuri()

--- src/AstarChicks.sol
+++ src/AstarChicks.sol
@@ -6,30 +6,9 @@ import {
 } from "./extensions/ERC721SeaDropSoulbound.sol";
 
 contract AstarChicks is ERC721SeaDropSoulbound {
-    string public constant uri = "";
     constructor(
         string memory name,
         string memory symbol,
         address[] memory allowedSeaDrop
     ) ERC721SeaDropSoulbound(name, symbol, allowedSeaDrop) {}
-
-    function tokenURI(
-        uint256 tokenId
-    ) public view virtual override returns (string memory) {
-        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
-
-        string memory baseURI = _baseURI();
-
-        // Exit early if the baseURI is empty.
-        if (bytes(baseURI).length == 0) {
-            return "";
-        }
-
-        // Check if the last character in baseURI is a slash.
-        if (bytes(baseURI)[bytes(baseURI).length - 1] != bytes("/")[0]) {
-            return baseURI;
-        }
-
-        return string(abi.encodePacked(baseURI, _toString(tokenId)));
-    }
 }
