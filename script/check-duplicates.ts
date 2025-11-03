d9efb83a9608f68d1f049dfbfe83f1743e383257
Mar.io <34627453+Maar-io@users.noreply.github.com>
2025-11-03 16:08:18 +0100

    adding missing addresses to round2

--- script/check-duplicates.ts
+++ script/check-duplicates.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 function checkDuplicatesInCsv(): void {
-  const csvPath = path.join(__dirname, '..', 'data', 'season2', 'test.csv');
+  const csvPath = path.join(__dirname, '..', 'data', 'season2', 'S2_allowlist2.csv');
   
   try {
     // Read the CSV file
