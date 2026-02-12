db3c158092911278f634b439134b3a8097808a5e
Marr.io <34627453+Maar-io@users.noreply.github.com>
2026-02-12 10:00:34 +0100

    Season5 (#7)
    * initial contract change for season5
    
    * deployment info
    
    * adding stage2 allowlist
    
    * add contract address

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ NFT Awarded for Soneium Score participants with 80+ points during each Season.
 * Season 2 Mint on [OpenSea](https://opensea.io/collection/soneium-score-season2-badge/overview), minted 55940(58089) tokens
 * Season 3 Mint on [OpenSea](https://opensea.io/collection/soneium-score-season3-badge/overview), minted 37564(38446) tokens
 * Season 4 Mint on [OpenSea](https://opensea.io/collection/soneium-score-season4-badge/overview), minted 0(27000) tokens
+* Season 5 Mint on [OpenSea](https://opensea.io/collection/soneium-score-season5-badge/overview), minted 0(27000) tokens
 
 ## Contracts
 
@@ -15,3 +16,4 @@ NFT Awarded for Soneium Score participants with 80+ points during each Season.
 * Score Season2 NFT on [Blockscout](https://soneium.blockscout.com/address/0x6b2f6d8216e075d3a71f4aaf21d7158af9b8dc82?tab=txs).
 * Score Season3 NFT on [Blockscout](https://soneium.blockscout.com/address/0x7BF02b42b9d4cCD85b497C9F53e6b7474f9c2546?tab=txs).
 * Score Season4 NFT on [Blockscout](https://soneium.blockscout.com/address/0x17121f9a7041FFe3EF248F7b84658d9229bad64f?tab=txs).
+* Score Season5 NFT on [Blockscout](https://soneium.blockscout.com/address/0xd8d14f829665183049707e0bdd93f9012bb3c4c2).
--- script/DeployAndConfigureScore.sol
+++ script/DeployAndConfigureScore.sol
@@ -7,7 +7,7 @@ pragma solidity 0.8.17;
 
 import "forge-std/Script.sol";
 
-import { ScoreSeason4 } from "../src/ScoreSeason4.sol";
+import { ScoreSeason5 } from "../src/ScoreSeason5.sol";
 
 import { ERC721SeaDrop } from "../src/ERC721SeaDrop.sol";
 
@@ -22,19 +22,19 @@ contract DeployAndConfigureScore is Script {
     address feeRecipient = 0xEE70e6d461F0888Fd9DB60cb5B2e933adF5f4c7C;
 
     // Token config
-    uint256 maxSupply = 26000;
+    uint256 maxSupply = 30000;
     string baseURI =
-        "ipfs://bafkreihwrclsa7kopy6hev5jzb4zhkwyz6dt5wqqlyukinhazvbuthv5sq"; // Score4 metadata pointing to image
+        "ipfs://bafkreibqhimdi2cchhhcjl2yvdfeaomojvecnrsdynyhel4mss2hoh5nba"; // Score5 metadata pointing to image
     string contractURI =
-        "ipfs://bafkreicqv7fupl7vxluh7g2fnosq5e45mcx3366gs5duhdto6xnbhsdcxm"; // Score4 contract info
+        "ipfs://bafkreif6qz4w7ydwv5wcwh3f6ut6uyvfh7qqtry6vwhxn3giy4uuuzmnoe"; // Score5 contract info
 
     // Drop config
     uint16 feeBps = 0;
-    uint80 mintPrice = 0 ether;
+    uint80 mintPrice = 1000 ether;
     uint16 maxTotalMintableByWallet = 1;
-    uint48 startTime = 1766386800; // Mon, 22 Dec 2025 07:00:00 GMT
-    uint48 endTime = 1767855600; // Thu, 08 Jan 2026 07:00:00 GMT
-    // uint48 endTimestage2 = 1769065200; // Thu, 22 Jan 2026 07:00:00 GMT
+    uint48 startTime = 1768806000; // Mon, January 19, 2026 7:00:00 AM
+    uint48 endTime = 1770015600; // Mon, February 2, 2026 7:00:00 AM
+    // uint48 endTimestage2 = 1771225200; // Mon, February 16, 2026 7:00:00 AM
 
 
     function run() external {
@@ -45,7 +45,7 @@ contract DeployAndConfigureScore is Script {
         address[] memory allowedSeadrop = new address[](1);
         allowedSeadrop[0] = seadrop;
 
-        ScoreSeason4 token = new ScoreSeason4();
+        ScoreSeason5 token = new ScoreSeason5();
 
         // Configure the token.
         token.setMaxSupply(maxSupply);
